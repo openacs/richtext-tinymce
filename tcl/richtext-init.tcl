ff9ae3651d4d179a00d3f36fa32c66f177041b03
Antonio Pisano <antonio@elettrotecnica.it>
2024-07-30 11:41:55 +0000

    Cleanup smelly comments and behavior that would be better implemented in xowiki when we will get rid of the ckeditor4 dependency (post 5.10.1 release)

--- tcl/richtext-init.tcl
+++ tcl/richtext-init.tcl
@@ -4,30 +4,7 @@ ad_library {
 
 ::richtext::tinymce::register_urns
 
-#
-# GN: is this the right place?
-#
 template::util::richtext::register_editor tinymce
-
-#
-# GN: this code is quite invasive. Why is this needed?
-#
-if {[apm_package_installed_p xowiki]} {
-    #
-    # We become the preferred richtext editor for xowiki, if none was
-    # chosen so far.
-    #
-    set preferred_editor [::parameter::get_global_value \
-                              -package_key xowiki \
-                              -parameter PreferredRichtextEditor]
-    if {$preferred_editor eq ""} {
-        ::parameter::set_global_value \
-            -package_key xowiki \
-            -parameter PreferredRichtextEditor \
-            -value "tinymce"
-    }
-}
-
 
 # Local variables:
 #    mode: tcl
