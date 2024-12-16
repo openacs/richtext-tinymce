5406f826361dadcf59a852f3856187f58013f3b7
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-12-16 12:23:54 +0000

    bump upstream version number from 7.5.1 to 7.6.0

--- tcl/richtext-procs.tcl
+++ tcl/richtext-procs.tcl
@@ -25,7 +25,7 @@ namespace eval ::richtext::tinymce {
     set parameter_info {
         package_key richtext-tinymce
         parameter_name Version
-        default_value 7.5.1
+        default_value 7.6.0
     }
 
     ad_proc resource_info {
